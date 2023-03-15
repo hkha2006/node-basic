23f258e54a1284dba6697b5fd5195543010ba219
hkha2006 <hkha2006@gmail.com>
2023-03-15 09:26:47 +0700

    commit-15-3
    add all model and migration

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -3,9 +3,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
       email: 'example@example.com',
+      password: '123456',
+      firstName: 'Dang',
+      lastName: 'Kha',
+      address: '273, ADV',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
