d6556cf85acad589d65122ab24b0386e1c6f2790
hkha2006 <hkha2006@gmail.com>
2023-04-11 09:59:22 +0700

    edit all code

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController"
+import doctorController from "../controllers/doctorController";
 let router = express.Router();
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
@@ -21,6 +22,8 @@ let initWebRoutes = (app) => {
     router.delete('/api/delete-user', userController.handleDeleteUser);
     router.put('/api/update-user', userController.handleUpdateUser);
     router.get('/api/allcode', userController.getAllCode);
+
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
     return app.use("/", router);
 }
 
