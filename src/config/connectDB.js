0a9bf8476fa1530c346fbd29968733d7b3399b6a
hkha2006 <hkha2006@gmail.com>
2023-05-15 10:09:59 +0700

    Revert "create folder build"
    This reverts commit 0fda6ebef8e33315851925a900735ac2e10b6c4d.

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,20 +1,10 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config()
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-        logging: false,
-        query: {
-            "raw": true
-        },
-        timezone: "+07:00",
-    });
+const sequelize = new Sequelize('hkha', 'root', null, {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+});
 
 let connectDB = async () => {
     try {
--- src/models/index.js
+++ src/models/index.js
@@ -5,32 +5,15 @@ import { basename as _basename, join } from 'path';
 import Sequelize, { DataTypes } from 'sequelize';
 const basename = _basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-const customizeConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  logging: false,
-  query: {
-    "raw": true
-  },
-  timezone: "+07:00"
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
-sequelize = new Sequelize(
-  process.env.DB_DATABASE_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  customizeConfig
-)
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 readdirSync(__dirname)
   .filter(file => {
